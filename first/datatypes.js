921cf411ebff72f79785a6aa6387d2f16a122773
Praneeth <107264304+Redpra@users.noreply.github.com>
2024-02-02 15:34:02 +0000

    2 done

---
+++ first/datatypes.js
@@ -0,0 +1,6 @@
+let a="hi hello" //string
+let k=true //boolean
+let j=18//integer
+//null- a standalone value-but here value is defined ie null(ie empty)-typeof null(is an object remember)
+//undefined-value is not defined
+console.log(typeof k)//gives the type
---
+++ first/var.js
@@ -0,0 +1,5 @@
+const i=10 //fixed we can't change its value again
+var j=20  //not used beacuse of its global scope
+let k=100 //frequentle used beacuse of local scope
+console.log(i)
+console.table([j,k])
