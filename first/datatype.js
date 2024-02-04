76d5eb7dbea400b0be91df48d0c4170678066191
Praneeth <107264304+Redpra@users.noreply.github.com>
2024-02-04 10:11:05 +0000

    datatypes done completely

---
+++ first/datatype.js
@@ -0,0 +1,20 @@
+//two types: PRIMITVE AND NON PRIMITIVE
+//PRIMITIVE-number,string,boolean,symbol,bigint,null,undefined
+let a=Symbol('123')
+let b=Symbol('123')
+console.log(a==b)
+//bigint-for declaring big numbers
+//null by default an object
+
+//NON PRIMITIVE-array,functions,object
+const t=[1,2,3,4];//arrays
+let v={
+    name:"praneeth",
+    age:19
+}//object decalrtion is donr in {} as key value pairs
+const myfun=function(){
+    console.log("hi")
+}
+
+
+
