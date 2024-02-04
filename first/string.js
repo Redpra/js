baa8037b766aa9bbb8492b384d70f0377c62355b
Praneeth <107264304+Redpra@users.noreply.github.com>
2024-02-04 10:54:54 +0000

    string functions

---
+++ first/string.js
@@ -0,0 +1,6 @@
+const str="hi"
+let s=new String("hi")
+let v=20
+console.log(`hi my name is ${str} and age will be ${v}`)//new type to use string
+//to know the types of functions in string use prototypes in console of google by showig the output
+//read once string functions
