d495ceefceb3dd651ba19e915d5b8599ef12c841
Praneeth <107264304+Redpra@users.noreply.github.com>
2024-02-04 09:32:24 +0000

    conversion done

---
+++ first/conversion.js
@@ -0,0 +1,20 @@
+let a="33"
+let num=Number(a)
+//console.log(num)
+//console.log(typeof(num))
+let b="37ab"
+let c=Number(b)
+//console.log(b)
+//console.log(typeof c)
+//console.log(c)
+//here if you wanna convert 37ab into number js will convert it into a number but if you print it ie c it shows Nan which mean not a number
+//similarly to convert into string we use String(value) and for boolean we use Boolean(value)
+let d=1
+var v=Boolean(d)
+//console.log(v)
+let w=""
+var v=Boolean(w)
+console.log(v)
+let n="pro"
+var v=Boolean(n)
+console.log(v)
